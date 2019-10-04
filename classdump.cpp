a98eaf8109c4b0379a50daf3a2a5652b12dfcba7
MuyuanL <56162314+MuyuanL@users.noreply.github.com>
2019-10-04 10:31:58 -0400

    Add files via upload

--- classdump.cpp
+++ classdump.cpp
@@ -5,5 +5,18 @@ Classdump::Classdump(char* fileName){
 }
 
 void Classdump::parsecp(void* start, int size){
-
+  void * ptr = start;
+  for (int i = 1; i < size; i ++){
+    byte tag = &(byte *) ptr;
+    ptr = (void*)((char* ptr) + 1);
+    switch (tag) {
+    case 1:
+      //utf8
+      short length = &(short*)ptr;
+      ptr = (void*)((short* ptr) + 1);
+      char* data = strndup((char*) ptr, length);
+      std::cout << "utf8 " << data << std::endl;
+      ptr = (void*)((char* ptr) + 1ength);
+    }
+  }
 }
